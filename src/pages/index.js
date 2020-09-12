256c78f84e34d5082fb4ae33d0e1f111c5f4007c
Charles Jambon <charles.jambon@fitle.com>
2020-09-12 15:07:29 +0200

    Add boucle logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,46 @@
 import React from "react"
+import styled, { createGlobalStyle } from "styled-components"
+
+import BCL_ROUGE from "../images/BCL_ROUGE.png"
+
+// Ajouter typo
+// Ajouter i18n
+//
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    min-width: 100vw;
+    min-height: 100vh;
+  }
+`
+
+const COLORS = {
+  main: "#304FFE",
+  secondary: "#FF5722",
+}
+
+const Layout = styled.div`
+  min-width: 100vw;
+  min-height: 100vh;
+  background-color: ${COLORS.main};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const MainLogo = styled.img`
+  width: 30%;
+`
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <GlobalStyle />
+      <Layout>
+        <MainLogo src={BCL_ROUGE} />
+      </Layout>
+    </>
+  )
 }
